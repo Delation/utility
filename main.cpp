3d7ed93256c2f0310c29ae7a6ba283f0ec10f498
Delation <88898324+Delation@users.noreply.github.com>
2021-08-17 10:34:26 -0500

    Fix program

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
-int n;
-std::cout << "Your number >>> ";
-std::cin >> n;
-for (i = 0;i < n + 1;i++) {
-  if (i == 0) {
-    continue;
-  }
-  if (n % i == 0) {
-    std::cout << n;
-    std::cout << " is divisble by ";
-    std::cout << i << std::endl;
-  }
+int main() {
+	int n;
+	std::cout << "Your number >>> ";
+	std::cin >> n;
+	for (int i = 0;i < n + 1;i++) {
+  	if (i == 0) {
+    	continue;
+  	}
+  	if (n % i == 0) {
+    	std::cout << n;
+   	 	std::cout << " is divisble by ";
+    	std::cout << i << std::endl;
+  	}
+  	}
+	return 0;
+}
