e41dac0db7b50ff57498fbb478e27064afbba711
Delation <88898324+Delation@users.noreply.github.com>
2021-08-17 14:37:15 -0500

    Fix indention

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ int main() {
 	std::cout << "Your number >>> ";
 	std::cin >> n;
 	for (int i = 0;i < n + 1;i++) {
-  	if (i == 0) {
-    	continue;
-  	}
-  	if (n % i == 0) {
-    	std::cout << n;
-   	 	std::cout << " is divisble by ";
-    	std::cout << i << std::endl;
-  	}
+  		if (i == 0) {
+    			continue;
+  		}
+  		if (n % i == 0) {
+    			std::cout << n;
+   	 		std::cout << " is divisble by ";
+    			std::cout << i << std::endl;
+  		}
   	}
 	return 0;
 }
