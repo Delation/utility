6addaced76eaad2baf9fc6311b4cdd8073bc61af
Delation <88898324+Delation@users.noreply.github.com>
2021-08-27 10:09:16 -0500

    Simplify

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@ int main() {
 	int n;
 	std::cout << "Your number >>> ";
 	std::cin >> n;
-	for (int i = 0;i < n + 1;i++) {
-  		if (i == 0) {
-    			continue;
-  		}
+	for (int i = 1;i < n + 1;i++) {
   		if (n % i == 0) {
     			std::cout << n;
    	 		std::cout << " is divisble by ";
