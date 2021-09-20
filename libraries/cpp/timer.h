125a11f8056d54fc024bfd18e481229c3379089e
Yeti <88898324+Delation@users.noreply.github.com>
2021-09-20 11:05:15 -0500

    Update timer.h

--- libraries/cpp/timer.h
+++ libraries/cpp/timer.h
@@ -1,10 +1,10 @@
+#pragma once
 #include <chrono>
 
 class Timer {
 	private:
-	  std::__ccr1::chrono::system_clock::time_point start_var = std::chrono::system_clock::now();
+	std::chrono::time_point<std::chrono::steady_clock> start_var;
 	public:
-	  Timer();
 	  void start();
-	  double get();
+	  float get();
 };
